9b59d9d01fa563fcaec9127040de2f3728768494
Norbert Kayumba <norbertkayumba0@gmail.com>
2025-08-30 05:32:57 +0300

    first commit

--- contact.php
+++
@@ -1,8 +0,0 @@
-
-   <?php
-$mess="<script> alert('Erreur ! Contectez-nous directement en cliquant sur le lien whatsapp ou email en bas de la page !')
-       location.href='index.php';
-       </script>"; 
-       echo $mess; 
-    
-?>
